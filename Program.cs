7afd78b72d0e10afbdcf5b23634dc1dcd72ea29d
Meredith Link <meredithlink@MacBook-Air.local>
2023-02-14 20:36:27 -0600

    final

--- Program.cs
+++ Program.cs
@@ -20,32 +20,67 @@ static void DisplayMenu() {
 }
 
 static bool IsValidChoice(string userInput) {
-    if(userInput == "1" || userInput =="3" || userInput == "3") {
+    if(userInput == "1" || userInput =="2" || userInput == "3") {
         return true;
     }
     else return false;
 }
 
 static void DisplayFull() {
-
+    Full();
+    PauseAction();
 }
 
 static void DisplayPartial() {
-
+    Partial();
+    PauseAction();
 }
 
 static void SayInvalid() {
-
+    Console.WriteLine("Invalid");
+    PauseAction();
 }
 
-static void RouteEm(int userInput) {
-    if(userInput==1){
+static void RouteEm(int menuChoice) {
+    if(menuChoice==1){
         DisplayFull();
     }
-    if(userInput==2) {
+    if(menuChoice==2) {
         DisplayPartial();
     }
-    if(userInput!=3) {
+    if(menuChoice!=3) {
         SayInvalid();
+    }
+}
+
+static void PauseAction() {
+    System.Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+}
+
+static void Full() {
+    Random rnd= new Random();
+    int rows = rnd.Next(2,10);
+
+    for(int i = 0; i <= rows; i++) {
+        for(int j = 0; j<= i; j++) {
+            Console.Write(0);
+        }
+        Console.WriteLine();
+    }
+}
+
+static void Partial() {
+    Random rnd= new Random();
+
+    for(int i = 3; i <= 9; i++) {
+        for(int j = 0; j < i; j++) {
+            if(rnd.Next(2) == 0) {
+                Console.Write(" ");
+            } else {
+                Console.Write("0");
+            }
+        }
+        Console.WriteLine();
     }
 }
